0a230399ac9298ab4c35527d6732113502e8c0b9
mary20230 <moracchinimary@gmail.com>
2021-08-10 17:50:59 +0200

    liaison contentful

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: "owf78d1kmg2l",
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: "oYfvnjguzwwhDBw4kLRP6Ff01fk0Qd6HiIq0FoaRw1Y"
+  });
+  console.log(client);
+ 
+
 //Variables
 
 const cartBtn = document.querySelector('.cart-btn');
@@ -21,6 +30,11 @@ let buttonsDOM = [];
 class Products {
     async getProducts() {
         try {
+
+            let contentful = await client.getEntries({
+                content_type: "comfyHouseProducts"
+            });
+            console.log(contentful);
             let result = await fetch('products.json');
             let data = await result.json();
             let products = data.items;
